baef6c8ebd0e7d100c9e2f981bc9768c1722058b
REIJI007 <miyajimareji007@gmail.com>
2024-09-17 02:03:29 +0800

    Update adblock_rule_generator_yaml.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator_yaml.ps1
+++ adblock_rule_generator_yaml.ps1
@@ -235,10 +235,9 @@ foreach ($url in $urlList) {
         $lines = $content -split "`n"
 
         foreach ($line in $lines) {
-            # 匹配以 @@|| 开头的规则，并提取域名
-            if ($line -match '^@@\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})*$') 
-            {
-                $domains = $line -replace '^@@\|\|', '' -split '\|'
+            # 匹配以 @@||、@@| 或 @@ 开头的规则，并提取域名
+            if ($line -match '^\s*@@\|\|?([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})*') {
+                $domains = $line -replace '^\s*@@\|\|?', '' -split '\|'
                 foreach ($domain in $domains) {
                     if ($domain.StartsWith('*')) {
                         $domain = $domain.Substring(1)
@@ -246,77 +245,34 @@ foreach ($url in $urlList) {
                     $excludedDomains.Add($domain) | Out-Null
                 }
             }
-            if ($line -match '^@@\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})?$') 
-            {
-                $domains = $line -replace '^@@\|\|', '' -split '\|'
-                foreach ($domain in $domains) {
-                    if ($domain.StartsWith('*')) {
-                        $domain = $domain.Substring(1)
-                    }
-                    $excludedDomains.Add($domain) | Out-Null
-                }
+            # 匹配 Adblock/Easylist 格式的规则
+            elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
+                $domain = $Matches[1]
+                $excludedDomains.Add($domain) | Out-Null
             }
-            # 匹配以 @@| 开头的规则
-            elseif ($line -match '^@@\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})*$') {
-                $domains = $line -replace '^@@\|', '' -split '\|'
-                foreach ($domain in $domains) {
-                    if ($domain.StartsWith('*')) {
-                        $domain = $domain.Substring(1)
-                    }
-                    $excludedDomains.Add($domain) | Out-Null
-                }
+            # 匹配 Hosts 文件格式的规则
+            elseif ($line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
+                $domain = $Matches[2]
+                $excludedDomains.Add($domain) | Out-Null
             }
-            elseif ($line -match '^@@\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})?$') {
-                $domains = $line -replace '^@@\|', '' -split '\|'
-                foreach ($domain in $domains) {
-                    if ($domain.StartsWith('*')) {
-                        $domain = $domain.Substring(1)
-                    }
-                    $excludedDomains.Add($domain) | Out-Null
-                }
+            # 匹配 Dnsmasq address=/域名/格式的规则
+            elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
+                $domain = $Matches[1]
+                $excludedDomains.Add($domain) | Out-Null
             }
-            # 匹配以 @@ 开头的规则
-            elseif ($line -match '^@@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})*$') {
-                $domains = $line -replace '^@@', '' -split '\|'
-                foreach ($domain in $domains) {
-                    if ($domain.StartsWith('*')) {
-                        $domain = $domain.Substring(1)
-                    }
-                    $excludedDomains.Add($domain) | Out-Null
-                }
+            # 匹配 Dnsmasq server=/域名/的规则
+            elseif ($line -match '^server=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
+                $domain = $Matches[1]
+                $excludedDomains.Add($domain) | Out-Null
             }
-            elseif ($line -match '^@@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})?$') {
-                $domains = $line -replace '^@@', '' -split '\|'
-                foreach ($domain in $domains) {
-                    if ($domain.StartsWith('*')) {
-                        $domain = $domain.Substring(1)
-                    }
-                    $excludedDomains.Add($domain) | Out-Null
-                }
+            # 匹配通配符规则
+            elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
+                $domain = $Matches[1]
+                $excludedDomains.Add($domain) | Out-Null
             }
             else {
-                # 匹配 Adblock/Easylist 格式的规则
+                # 将不符合以上模式的规则添加到 uniqueRules
                 if ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
-                    $domain = $Matches[1]
-                    $uniqueRules.Add($domain) | Out-Null
-                }
-                # 匹配 Hosts 文件格式的规则
-                elseif ($line -match '^(0\.0\.0\.0|127\.0\.0\.1)\s+([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$') {
-                    $domain = $Matches[2]
-                    $uniqueRules.Add($domain) | Out-Null
-                }
-                # 匹配 Dnsmasq address=/域名/格式的规则
-                elseif ($line -match '^address=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
-                    $domain = $Matches[1]
-                    $uniqueRules.Add($domain) | Out-Null
-                }
-                # 匹配 Dnsmasq server=/域名/的规则
-                elseif ($line -match '^server=/([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/$') {
-                    $domain = $Matches[1]
-                    $uniqueRules.Add($domain) | Out-Null
-                }
-                # 匹配通配符规则
-                elseif ($line -match '^\|\|([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\^$') {
                     $domain = $Matches[1]
                     $uniqueRules.Add($domain) | Out-Null
                 }
@@ -331,6 +287,7 @@ foreach ($url in $urlList) {
 
 # 排除以 @@||、@@| 和 @@ 开头规则中提取的域名
 $finalRules = $uniqueRules | Where-Object { -not $excludedDomains.Contains($_) }
+
 
 
 
